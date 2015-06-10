9213147ff806f41ac99a3af9a0d9620d9740d4ab
lianzhao <knife.fe@gmail.com>
2015-06-10 13:56:49 +0800

    remove 3rd party dependency

--- util.ts
+++ util.ts
@@ -1,22 +1,21 @@
-/// <reference path="typings/encoding.ts"/>
 function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
   var bufView = new Uint16Array(buf);
-  for (var i=0, strLen=str.length; i < strLen; i++) {
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
   return buf;
 }
 
 function uInt8Array2String(array: Uint8Array) {
-  return String.fromCharCode.apply(null, array);  
+  return String.fromCharCode.apply(null, array);
 }
 
 function uInt16Array2String(array: Uint16Array) {
-  return String.fromCharCode.apply(null, array);  
+  return String.fromCharCode.apply(null, array);
 }
 
 //if (!XMLHttpRequest.prototype.sendAsBinary) {
@@ -31,13 +30,13 @@ function uInt16Array2String(array: Uint16Array) {
 //  };
 //}
 
-function string2Uint8Array(str: string):Uint8Array {
-//  var nBytes = str.length;
-//  var ui8Data = new Uint8Array(nBytes);
-//    for (var nIdx = 0; nIdx < nBytes; nIdx++) {
-//      ui8Data[nIdx] = str.charCodeAt(nIdx) & 0xff;
-//    }
-//    return ui8Data;
-var encoder = new TextEncoder("UTF-8");
-return encoder.encode(str);
+function string2Uint8Array(str: string): Uint8Array {
+  //  var nBytes = str.length;
+  //  var ui8Data = new Uint8Array(nBytes);
+  //    for (var nIdx = 0; nIdx < nBytes; nIdx++) {
+  //      ui8Data[nIdx] = str.charCodeAt(nIdx) & 0xff;
+  //    }
+  //    return ui8Data;
+  var encoder = new TextEncoder("UTF-8");
+  return encoder.encode(str);
 }
