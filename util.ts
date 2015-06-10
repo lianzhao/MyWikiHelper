a0891b2578fe64e793924a154d7dd874bba4ab9e
lianzhao <knife.fe@gmail.com>
2015-06-10 13:46:48 +0800

    fix encoding issues

--- util.ts
+++ util.ts
@@ -1,3 +1,4 @@
+/// <reference path="typings/encoding.ts"/>
 function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
@@ -31,10 +32,12 @@ function uInt16Array2String(array: Uint16Array) {
 //}
 
 function string2Uint8Array(str: string):Uint8Array {
-  var nBytes = str.length;
-  var ui8Data = new Uint8Array(nBytes);
-    for (var nIdx = 0; nIdx < nBytes; nIdx++) {
-      ui8Data[nIdx] = str.charCodeAt(nIdx) & 0xff;
-    }
-    return ui8Data;
+//  var nBytes = str.length;
+//  var ui8Data = new Uint8Array(nBytes);
+//    for (var nIdx = 0; nIdx < nBytes; nIdx++) {
+//      ui8Data[nIdx] = str.charCodeAt(nIdx) & 0xff;
+//    }
+//    return ui8Data;
+var encoder = new TextEncoder("UTF-8");
+return encoder.encode(str);
 }
